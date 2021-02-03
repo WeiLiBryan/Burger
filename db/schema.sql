08910a65b41afa5d69662cb152592eb9b27e2b0f
William Bryan <wwbryan00@gmail.com>
2021-02-02 21:30:25 -0800

    add database creation to schema.sql

---
+++ db/schema.sql
@@ -0,0 +1,11 @@
+DROP DATABASE IF EXISTS burgers_db;
+
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+CREATE TABLE burgers(
+    id int AUTO_INCREMENT PRIMARY KEY,
+    burger_name VARCHAR(50),
+    devoured BOOLEAN
+);
