7153be34522233463d9295eeb1d4404e73fbe5d2
William Bryan <wwbryan00@gmail.com>
2021-02-03 01:48:05 -0800

    add form to index.handlebars

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -9,7 +9,6 @@ router.get("/", function (req, res) {
         var tempObj = {
             burger: data
         };
-        console.log(tempObj);
         res.render("index", tempObj);
     });
 });
@@ -27,8 +26,6 @@ router.post("/api/burgers", function (req, res) {
 
 router.put("/api/burgers/:id", function (req, res) {
     var condition = "id = " + req.params.id;
-
-    console.log("condition", condition);
 
     burger.update({
         devoured: req.body.devoured
